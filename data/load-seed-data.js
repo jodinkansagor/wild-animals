50cc1b8c25d5d95b458d4a3c2a9cdca174295351
Jodi N Kansagor <joedvice@gmail.com>
2019-11-06 17:48:54 -0800

    added script files to data folder

---
+++ data/load-seed-data.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const pg = require('pg');
+const Client = pg.Client;
+// import seed data:
+const animals = require('./data/see-data');
+
+run();
+
+async function run() {
+    const client = new Client(process.env.DATABASE_URL);
+
+    try {
+        await client.connect();
+    
+        // "Promise all" does a parallel execution of async tasks
+        await Promise.all(
+            // map every item in the array data
+            animals.map(animal => {
+
+                return client.query(`
+                    INSERT INTO animals (name, weight, image, type, hasHair)
+                    VALUES ($1, $2, $3, $4, $5);
+                    `,
+                [animal.name, animal.weight, animal.image, animal.type, animal.hasHair]);
+                
+                
+                
+            })
+        );
+
+        console.log('seed data load complete');
+    }
+    catch (err) {
+        console.log(err);
+    }
+    finally {
+        client.end();
+    }
+    
+}
